4558985d994cbeb34876c76447b7ac7d1874488f
yechoi42 <yechoi@student.42seoul.kr>
2022-04-29 00:09:07 +0900

    week4 ex03

---
+++ src/week4/animal/Animal.java
@@ -0,0 +1,31 @@
+package week4.animal;
+
+/*
+다형성(polymorphism)이란?
+하나의 객체가 여러 가지 타입을 가질 수 있는 것
+부모 클래스 타입의 참조 변수로 자식 클래스 타입의 인스턴스 참조
+ */
+public abstract class Animal {
+    abstract void move();
+}
+
+class Human extends Animal {
+    @Override
+    void move() {
+        System.out.println("사람이 두 발로 걷습니다.");
+    }
+}
+
+class Tiger extends Animal {
+    @Override
+    void move() {
+        System.out.println("호랑이가 네 발로 뜁니다.");
+    }
+}
+
+class Eagle extends Animal {
+    @Override
+    void move() {
+        System.out.println("독수리가 하늘을 날아갑니다.");
+    }
+}
---
+++ src/week4/animal/AnimalTest.java
@@ -0,0 +1,27 @@
+package week4.animal;
+import java.util.ArrayList;
+
+public class AnimalTest {
+    public static void main(String[] args) {
+        Animal hAnimal = new Human();
+        Animal tAnimal = new Tiger();
+        Animal eAnimal = new Eagle();
+
+        AnimalTest test = new AnimalTest();
+        test.moveAnimal(hAnimal);
+        test.moveAnimal(tAnimal);
+        test.moveAnimal(eAnimal);
+
+        ArrayList<Animal> animalList = new ArrayList<Animal>();
+        animalList.add(hAnimal);
+        animalList.add(tAnimal);
+        animalList.add(eAnimal);
+
+        for(Animal animal : animalList) {
+            animal.move();
+        }
+    }
+    public void moveAnimal(Animal animal) {
+        animal.move();
+    }
+}
