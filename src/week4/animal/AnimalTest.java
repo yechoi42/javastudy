c788768361637cc044c5f87db5c2c5c3fa9affe0
yechoi42 <yechoi@student.42seoul.kr>
2022-05-03 23:56:17 +0900

    week4 ex07

--- src/week4/animal/AnimalTest.java
+++ src/week4/animal/AnimalTest.java
@@ -1,6 +1,24 @@
 package week4.animal;
 import java.util.ArrayList;
 
+/*
+상속(Inheritance)을 쓰는 이유
+- IS-A 관계
+- 모듈화를 통한 재사용
+- 코드 간결
+
+상속의 단점 및 한계점
+- 캡슐화가 깨지고 결합도가 높아짐
+- 유연성, 확장성 떨어짐 : 컴파일 시점에 부모-자식 관계 결정돼 구현에 의존
+- 다중 상속 문제, 클래스 폭발 문제
+c.f. https://mangkyu.tistory.com/199
+
+합성(Composition)이란?
+- Has-A 관계
+- 중복된 로직을 갖는 객체를 구현, 이 객체를 주입받아 중복 로직 호출
+- 의존하는 객체를 교체하는 것이 비교적 쉬우므로 설계가 유연
+
+ */
 public class AnimalTest {
     public static void main(String[] args) {
         Animal hAnimal = new Human();
